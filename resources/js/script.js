a4006ad26a4882bf6b0bdeec04b87314107a6d5d
Zach Panzer <zach@finehumans.com>
2017-01-18 15:53:35 -0500

    design update

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,13 +1,48 @@
+
+// Google Map
+
 function initMap() {
+
+  // Google Map Styles
+
+    var styles = [ {
+
+      stylers: [
+        { hue: '#d7534c'},
+        { saturation: -20 }
+
+        ]},
+
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [
+            { lightness: 50 },
+            { visability: 'simplified' }
+
+          ]
+        }, 
+
+        {
+
+          featureType: 'road',
+          elementType: 'labels',
+          stylers: [
+            { visibility: 'off' }
+          ]
+        }
+    ];
+
 	 var map = new google.maps.Map(document.getElementById('map'), {
-    	center: {lat: -34.397, lng: 150.644},
-    	zoom: 8,
+    	center: {lat: 40.454983, lng: -80.012403},
+    	zoom: 15,
     	zoomControl: false,
   		mapTypeControl: false,
   		scaleControl: false,
   		streetViewControl: false,
   		rotateControl: false,
-  		fullscreenControl: false
+  		fullscreenControl: false,
+      styles: styles
 
   	});
-}
+} // end Google Map
